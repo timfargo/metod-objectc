a3bc6a906391d8d34de6780b0728cbd3cdf3cda0
timfargo <isalintimur981@gmail.com>
2022-06-04 20:08:05 +0300

    метод-объекты

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+public class Book {
+
+    private String title;
+    private Author author;
+    private int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Книга \"" + title + "\", год издания: " + year + ",автор: " + author.toString();
+    }
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Астрид","Линдгрен");
+        Author author2 = new Author("Эрнест","Шепард");
+
+        Book book1 = new Book("Малыш и Карлосон", author1, 1955);
+        Book book2 = new Book("Винни-Пух",author2,1926);
+
+        System.out.println(author1.hashCode());
+        System.out.println(author2.hashCode());
+
+        System.out.println(author1.equals(author2));
+        System.out.println(author2.equals(author1));
+
+        System.out.println(author1);
+        System.out.println(author2);
+
+        System.out.println(book1.hashCode());
+        System.out.println(book2.hashCode());
+
+        System.out.println(book1.equals(book2));
+        System.out.println(book2.equals(book1));
+
+        System.out.println(book1);
+        System.out.println(book2);
+
     }
 }
